42a53415e3838192e9495f0f9281ba2873cff3cb
Krishtopa Maksim <k.maks21@mail.ru>
2024-05-07 16:44:42 +0700

    Первый успешный тест cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe ('Cypress Tests', () =>{
+  it('None-existent login test', () =>{
+      cy.fixture('cypressTest').then(data => {
+          cy.log('Переход на страницу авторизации')
+          cy.visit(data.auth_url)
+
+          cy.log('Ввод несуществующего логина')
+          cy.get('.form-input--text')
+              .type(data.login)
+
+          cy.log('Клик по кнопке  "Войти"')
+          cy.get('div[class="passp-button passp-sign-in-button"] button[type="submit"]')
+              .click()
+              
+          cy.log('Проверка что появился элемент сигнализирющий об ошибке')
+          cy.get('div[class="passp=form-field__error"]')
+              .should('exist')
+      })
   })
-})
+});
